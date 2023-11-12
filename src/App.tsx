2fb8f586c9ca03481b64c5a64e5c93391b145095
AshKemp <kmuraliashwin@gmail.com>
2023-11-12 09:44:37 +0530

    use type predicate

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,10 @@ function App() {
       {/* <Input id="name" label="Your name" type="text" />
       <Input id="age" label="Your age" type="number" /> */}
       <p>
-        <Button element="anchor" href="https://www.google.com">
-          A Link
-        </Button>
+        <Button href="https://www.google.com">A Link</Button>
       </p>
       <p>
-        <Button element="button">A Button</Button>
+        <Button>A Button</Button>
       </p>
     </main>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,17 @@
 import { ComponentPropsWithoutRef } from "react";
 
-type ButtonProps = {
-  element: "button";
-} & ComponentPropsWithoutRef<"button">;
+type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  href?: never;
+};
+type AnchorProps = ComponentPropsWithoutRef<"a"> & {
+  href?: string;
+};
 
-type AnchorProps = {
-  element: "anchor";
-} & ComponentPropsWithoutRef<"a">;
+function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps {
+  return "href" in props;
+}
 export default function Button(props: ButtonProps | AnchorProps) {
-  if (props.element === "anchor") {
+  if (isAnchorProps(props)) {
     return <a className="button" {...props}></a>;
   }
   return <button className="button" {...props}></button>;
