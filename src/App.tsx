02960ad1bdef12113c4fe67b1b2fd8196b458348
AshKemp <kmuraliashwin@gmail.com>
2023-11-12 12:55:00 +0530

    use forwardRef with React

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import Button from "./components/Button";
-import Container from "./components/Container";
-// import Input from "./components/Input";
+// import Button from "./components/Button";
+// import Container from "./components/Container";
+import { useRef } from "react";
+import Input from "./components/Input";
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
   return (
     <main>
-      {/* <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" /> */}
+      {/* <Input id="name" label="Your name" type="text" /> */}
+      {/* <Input id="age" label="Your age" type="number" /> */}
+      <Input id="test" label="Test" ref={input} />
       {/* <p>
         <Button href="https://www.google.com">A Link</Button>
       </p>
       <p>
         <Button>A Button</Button>
       </p> */}
-      <Container as={Button} onClick={() => console.log("Button clicked")}>
+      {/* <Container as={Button} onClick={() => console.log("Button clicked")}>
         Click Me
-      </Container>
+      </Container> */}
     </main>
   );
 }
