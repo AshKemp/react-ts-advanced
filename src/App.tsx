c858cda68711ffe3d1e15df99ed3f1d408f80f27
AshKemp <kmuraliashwin@gmail.com>
2023-11-12 09:21:33 +0530

    Wrapper component renders different elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import Input from "./components/Input";
+import Button from "./components/Button";
+// import Input from "./components/Input";
 
 function App() {
   return (
     <main>
-      <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" />
+      {/* <Input id="name" label="Your name" type="text" />
+      <Input id="age" label="Your age" type="number" /> */}
+      <p>
+        <Button element="anchor" href="https://www.google.com">
+          A Link
+        </Button>
+      </p>
+      <p>
+        <Button element="button">A Button</Button>
+      </p>
     </main>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { ComponentPropsWithoutRef } from "react";
+
+type ButtonProps = {
+  element: "button";
+} & ComponentPropsWithoutRef<"button">;
+
+type AnchorProps = {
+  element: "anchor";
+} & ComponentPropsWithoutRef<"a">;
+export default function Button(props: ButtonProps | AnchorProps) {
+  if (props.element === "anchor") {
+    return <a className="button" {...props}></a>;
+  }
+  return <button className="button" {...props}></button>;
+}
